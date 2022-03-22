a713c24badb045f390a352ce50007c709f5e3f4d
Alexis-Looten <90459001+Alexis-Looten@users.noreply.github.com>
2022-03-22 10:55:45 +0100

    Add files via upload

---
+++ hangman/hangman_test.go
@@ -0,0 +1,71 @@
+package hangman
+
+import "testing"
+
+func TestLetterInWord(t *testing.T) {
+	word := []string{"b", "o", "b"}
+	guess := "b"
+	hasLetter := letterInWord(guess, word)
+	if !hasLetter {
+		t.Errorf("Word %s contains letter %s. got=%v", word, guess, hasLetter)
+	}
+}
+
+func TestLetterNotInWord(t *testing.T) {
+	word := []string{"b", "o", "b"}
+	guess := "a"
+	hasLetter := letterInWord(guess, word)
+	if hasLetter {
+		t.Errorf("Word %s does not contain letter %s. got=%v", word, guess, hasLetter)
+	}
+}
+
+func TestInvalidWord(t *testing.T) {
+	_, err := New(3, "")
+	if err == nil {
+		t.Errorf("Error should be return when using an invalid word=''")
+	}
+}
+
+func TestGameGoodGuess(t *testing.T) {
+	g, _ := New(3, "bob")
+	g.MakeAGuess("b")
+	validState(t, "goodGuess", g.State)
+}
+
+func TestGameAlreadyGuessed(t *testing.T) {
+	g, _ := New(3, "bob")
+	g.MakeAGuess("b")
+	g.MakeAGuess("b")
+	validState(t, "alreadyGuessed", g.State)
+}
+
+func TestGameBadGuess(t *testing.T) {
+	g, _ := New(3, "bob")
+	g.MakeAGuess("a")
+	validState(t, "badguess", g.State)
+}
+
+func TestGameWon(t *testing.T) {
+	g, _ := New(3, "bob")
+	g.MakeAGuess("b")
+	g.MakeAGuess("o")
+	g.MakeAGuess("b")
+	validState(t, "won", g.State)
+}
+
+func TestGameLost(t *testing.T) {
+	g, _ := New(3, "bob")
+	g.MakeAGuess("a")
+	g.MakeAGuess("c")
+	g.MakeAGuess("d")
+	validState(t, "lost", g.State)
+}
+
+func validState(t *testing.T, expectedState, actuelState string) bool {
+	if expectedState != actuelState {
+		t.Errorf("state should be '%v'. got=%v", expectedState, actuelState)
+		return false
+	}
+	return false
+}
